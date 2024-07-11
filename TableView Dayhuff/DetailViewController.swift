855cbb6db3014421a26319789cbb80568d7b00d3
Cliffhang449 <noahdayhuff@gmail.com>
2024-07-11 02:32:35 -0500

    Final (ReUpload)

--- TableView Dayhuff/DetailViewController.swift
+++ TableView Dayhuff/DetailViewController.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemDetailsLabel: UILabel!
+    
+    var itemName: String?
+    var itemDetails: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLabels()
+        }
+    
+    func updateLabels() {
+        if let itemName = itemName {
+            itemNameLabel.text = itemName
+        }
+        if let itemDetails = itemDetails {
+            itemDetailsLabel.text = itemDetails
+        }
+    }
+}
--- TableView Dayhuff/ViewController.swift
+++ TableView Dayhuff/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let items = [
+            Item(name: "Cosmodrome", details: "Exotic: Anarchy\nRaid: Scourge of the Past"),
+            Item(name: "Dreadnought", details: "Exotic: Touch of Malice\nRaid: King's Fall"),
+            Item(name: "Dreaming City", details: "Exotic: One Thousand Voices\nRaid: Last Wish"),
+            Item(name: "Europa", details: "Exotic: Eyes of Tomorrow\nRaid: Deep Stone Crypt"),
+            Item(name: "Moon", details: "Exotic: Divinity\nRaid: Garden of Salvation"),
+            Item(name: "Venus", details: "Exotic: Vex Mythoclast\nRaid: Vault of Glass")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row].name
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationVC = segue.destination as! DetailViewController
+                destinationVC.itemName = items[indexPath.row].name
+                destinationVC.itemDetails = items[indexPath.row].details
+            }
+        }
+    }
 }
 
